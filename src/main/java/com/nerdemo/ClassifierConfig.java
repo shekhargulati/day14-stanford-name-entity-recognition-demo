d12e20d1b49e19322cf38245e545f0fe85663605
Shekhar Gulati <shekhargulati84@gmail.com>
2013-11-04 01:08:26 +0530

    fixed bean issue

--- src/main/java/com/nerdemo/ClassifierConfig.java
+++ src/main/java/com/nerdemo/ClassifierConfig.java
@@ -1,5 +1,6 @@
 package com.nerdemo;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.inject.Named;
@@ -7,15 +8,21 @@ import javax.inject.Named;
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.ling.CoreLabel;
 
+@ApplicationScoped
 public class ClassifierConfig {
 
     private String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
+    private CRFClassifier<CoreLabel> classifier;
+
+    @PostConstruct
+    public void postConstruct() {
+        CRFClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
+        this.classifier = classifier;
+    }
 
     @Produces
     @Named
-    @ApplicationScoped
-    public CRFClassifier<CoreLabel> classifier(){
-        CRFClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
+    public CRFClassifier<CoreLabel> classifier() {
         return classifier;
     }
 }
