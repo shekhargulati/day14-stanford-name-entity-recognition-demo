3bd2d05c423682e4212a9bed844eba4979a98242
Shekhar Gulati <shekhargulati84@gmail.com>
2013-11-04 00:57:34 +0530

    bean was created for every request. In resource only adding ner values to list

--- src/main/java/com/nerdemo/ClassifierConfig.java
+++ src/main/java/com/nerdemo/ClassifierConfig.java
@@ -7,13 +7,13 @@ import javax.inject.Named;
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.ling.CoreLabel;
 
-@ApplicationScoped
 public class ClassifierConfig {
 
     private String serializedClassifier = "classifiers/english.all.3class.distsim.crf.ser.gz";
 
     @Produces
     @Named
+    @ApplicationScoped
     public CRFClassifier<CoreLabel> classifier(){
         CRFClassifier<CoreLabel> classifier = CRFClassifier.getClassifierNoExceptions(serializedClassifier);
         return classifier;
--- src/main/java/com/nerdemo/ClassifierRestResource.java
+++ src/main/java/com/nerdemo/ClassifierRestResource.java
@@ -7,27 +7,31 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import edu.stanford.nlp.ie.crf.CRFClassifier;
-import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreLabel;
 
 @Path("/classify")
 public class ClassifierRestResource {
-    
+
     @Inject
     private CRFClassifier<CoreLabel> classifier;
 
     @GET
-    @Path("/{text}")
-    public List<Result> findNer(@PathParam("text") String text){
+    @Path(value = "/{text}")
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public List<Result> findNer(@PathParam("text") String text) {
         List<List<CoreLabel>> classify = classifier.classify(text);
         List<Result> results = new ArrayList<>();
         for (List<CoreLabel> coreLabels : classify) {
             for (CoreLabel coreLabel : coreLabels) {
                 String word = coreLabel.word();
-                String answer = coreLabel.getString(CoreAnnotations.AnswerAnnotation.class);
-                results.add(new Result(word, answer));
+                String answer = coreLabel.ner();
+                if(answer != null){
+                    results.add(new Result(word, answer));
+                }
             }
         }
         return results;
